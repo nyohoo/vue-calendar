b0db166e554c7e77d374d66b71800cb804a16700
nyohoo <nyohoo2013@gmail.com>
2022-06-16 03:41:28 +0900

    add: seedの作成

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Event.create!(name: '予定1', start: '2022-06-16 10:00:00', end: '2022-06-16 11:00:00')
+Event.create!(name: '予定2', start: '2022-06-16 10:00:00', end: '2022-06-16 11:00:00')
+Event.create!(name: '予定3', start: '2022-06-16 10:00:00', end: '2022-06-16 11:00:00')
+Event.create!(name: '予定4', start: '2022-06-16 10:00:00', end: '2022-06-16 11:00:00')
