27961dd01336bbc59f6b8cd47b62b7b51de48c40
nyohoo <nyohoo2013@gmail.com>
2022-06-16 10:27:00 +0900

    fix: ルーティングのリファクタリング

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get '/events', to: 'events#index'
-  get '/events/:id', to: 'events#show'
-  post '/events', to: 'events#create'
-  put '/events/:id', to: 'events#update'
-  delete '/events/:id', to: 'events#destroy'
+  resources :events, only: [:index, :show, :create, :update, :destroy]
 end
