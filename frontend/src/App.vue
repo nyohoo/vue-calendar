73856cae40fcaa805788fc452d8538e062cabd05
nyohoo <nyohoo2013@gmail.com>
2022-06-16 16:44:10 +0900

    add: axiosを利用してAPIサーバからデータ取得テスト

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld message="Hello World!" />
+    <Calendar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Calendar from './components/Calendar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-  },
+    Calendar
+  }
 };
 </script>
---
+++ frontend/src/components/Calendar.vue
@@ -0,0 +1,33 @@
+  <template>
+    <div>
+      <h1>Calendar</h1>
+      <p>events:</p>
+      <p>{{ events }}</p>
+      
+      <button type="submit" @click="fetchEvents()">fetchEvents</button>
+
+    </div>
+  </template>
+
+  <script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Calendar',
+    data: () => ({
+      events: []
+    }),
+    methods: {
+      fetchEvents() {
+        axios
+          .get('http://localhost:3000/events')
+          .then(response => {
+            this.events = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    }
+  };
+  </script>
