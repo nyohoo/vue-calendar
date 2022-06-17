e0dba54e1c5412c5efd0776d263eab543c83a9b6
nyohoo <nyohoo2013@gmail.com>
2022-06-17 19:22:34 +0900

    add: カレンダーボタンの追加　日本語化

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,25 +1,42 @@
 <template>
   <div>
-    <v-sheet height="100vh">
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-btn icon @click="$refs.calendar.prev()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn icon @click="$refs.calendar.next()">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
       <v-calendar
+        ref="calendar"
         v-model="value"
         :events="events"
         @change="fetchEvents"
+        locale="ja-jp"
+        :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+        :month-format="(timestamp) => (new Date(timestamp.date).getMonth() + 1) + ' /'"
       ></v-calendar>
     </v-sheet>
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns';
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'Calendar',
   data: () => ({
-    value: new Date('2022/06/01'),
+    value: format(new Date(), 'yyyy/MM/dd'),
   }),
   computed: {
     ...mapGetters('events', ['events']),
+    title() {
+      return format(new Date(this.value), 'yyyy年 M月');
+    }
   },
   methods: {
     ...mapActions('events', ['fetchEvents']),
