cc0c3bf003309e1add15e423ca481876f42beecc
nyohoo <nyohoo2013@gmail.com>
2022-06-18 04:57:14 +0900

    add: ダイアログの見た目を整える

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -23,19 +23,48 @@
       ></v-calendar>
     </v-sheet>
 
-    <v-dialog :value="event !== null">
+    <v-dialog :value="event !== null" width="600">
       <div v-if="event !== null">
-        <v-card>
-          <h1>イベント詳細</h1>
-          <p>name: {{ event.name }}</p>
-          <p>start: {{ event.start.toLocaleString() }}</p>
-          <p>end: {{ event.end.toLocaleString() }}</p>
-          <p>timed: {{ event.timed }}</p>
-          <p>description: {{ event.description }}</p>
-          <p>color: {{ event.color }}</p>
+        <v-card class="pb-12">
+          <v-card-actions class="d-flex justify-end pa-2">
+            <v-btn icon>
+              <v-icon size="20px">mdi-close</v-icon>
+            </v-btn>
+          </v-card-actions>
+          <v-card-title>
+            <v-row>
+              <v-col cols="2" class="d-flex justify-center align-center">
+                <v-icon size="20px" :color="event.color || 'blue'">mdi-square</v-icon>
+              </v-col>
+              <v-col class="d-flex align-center">
+                {{ event.name }}
+              </v-col>
+            </v-row>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="2" class="d-flex justify-center align-center">
+                <v-icon size="20px">mdi-clock-time-three-outline</v-icon>
+              </v-col>
+              <v-col class="d-flex align-center">
+                {{ event.start.toLocaleString() }} ~ {{ event.end.toLocaleString() }}
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-text>
+            <v-row>
+              <v-col cols="2" class="d-flex justify-center align-center">
+                <v-icon size="20px">mdi-card-text-outline</v-icon>
+              </v-col>
+              <v-col class="d-flex align-center">
+                {{ event.description || 'no description' }}
+              </v-col>
+            </v-row>
+          </v-card-text>
         </v-card>
       </div>
     </v-dialog>
+
   </div>
 </template>
 
