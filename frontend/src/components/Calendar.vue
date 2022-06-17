5ad589d98f4a141f50a66100c881fc1a4d1d3020
nyohoo <nyohoo2013@gmail.com>
2022-06-17 17:35:56 +0900

    add: vuetifyでカレンダーを表示

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
-    <h1 class="text-h1">Calendar</h1>
-    <v-list>
-      <v-list-item v-for="event in events" :key="event.id">
-        {{ event.name }}
-      </v-list-item>
-    </v-list>
-    <v-btn type="submit" @click="fetchEvents()">fetchEvents</v-btn>
-
-    <CalendarDetails />
+    <v-sheet height="100vh">
+      <v-calendar></v-calendar>
+    </v-sheet>
   </div>
 </template>
 
