ce51eb19cb25b8724f9fdb90707651e9c8ae6412
nyohoo <nyohoo2013@gmail.com>
2022-06-18 23:34:20 +0900

    add: EventFormDialogコンポーネントをCalendarコンポーネントで呼び出して表示

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -20,11 +20,13 @@
         :day-format="(timestamp) => new Date(timestamp.date).getDate()"
         :month-format="(timestamp) => (new Date(timestamp.date).getMonth() + 1) + ' /'"
         @click:event="showEvent"
+        @click:day="initEvent"
       ></v-calendar>
     </v-sheet>
 
     <v-dialog :value="event !== null" @click:outside="closeDialog" width="600">
       <EventDetailDialog v-if="event !== null" />
+      <EventFormDialog v-if="event !== null" />
     </v-dialog>
 
   </div>
@@ -34,12 +36,14 @@
 import { format } from 'date-fns';
 import { mapGetters, mapActions } from 'vuex';
 import EventDetailDialog from './EventDetailDialog';
+import EventFormDialog from './EventFormDialog.vue';
 
 export default {
     name: "Calendar",
     components: {
-      EventDetailDialog,
-    },
+    EventDetailDialog,
+    EventFormDialog,
+},
     data: () => ({
         value: format(new Date(), "yyyy/MM/dd"),
     }),
@@ -54,12 +58,19 @@ export default {
         setToday() {
             this.value = format(new Date(), "yyyy/MM/dd");
         },
-        showEvent({ event }) {
+        showEvent({ nativeEvent, event }) {
             this.setEvent(event);
+            nativeEvent.stopPropagation();
         },
         closeDialog() {
             this.setEvent(null);
-        }
+        },
+        initEvent({ date }) {
+          date = date.replace(/-/g,'/');
+          const start = format(new Date(date), 'yyyy/MM/dd 00:00:00')
+          const end = format(new Date(date), 'yyyy/MM/dd 01:00:00')
+          this.setEvent({ name: '', start, end, timed: true });
+        },
     },
 };
 </script>
