c6959be3be836bcf00366862163c0d78fbcc1dbe
nyohoo <nyohoo2013@gmail.com>
2022-06-19 04:09:47 +0900

    add:add: データフォーム部分をコンポーネントとして切り出し

---
+++ frontend/src/components/DateForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-date-picker 
+      :value="value"
+      @input="$emit('input', $event)"
+      no-title
+      locale="ja-ja"
+      :day-format="value => new Date(value).getDate()"
+    ></v-date-picker>
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DateForm',
+  props: ['value'],
+}
+</script>
