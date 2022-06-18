744cbe6fc26782e6e637407dc6075aeeeba16086
nyohoo <nyohoo2013@gmail.com>
2022-06-18 23:34:00 +0900

    add: イベント作成用のフォーム作成

---
+++ frontend/src/components/EventFormDialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="pb-12">
+    <v-card-actions class="d-flex justify-end pa-2">
+      <v-btn icon @click="closeDialog">
+        <v-icon size="20px">mdi-close</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-card-text>
+      <DialogSection icon="mdi-square" :color="event.color || 'blue'">
+        <v-text-field v-model="name" label="タイトル"></v-text-field>
+      </DialogSection>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import DialogSection from "./DialogSection.vue";
+
+export default {
+  name: 'EventFormDialog',
+  components: {
+    DialogSection,
+  },
+  data: () => ({
+    name: '',
+  }),
+  computed: {
+    ...mapGetters('events', ['event']),
+  },
+  methods: {
+    ...mapActions('events', ['setEvent']),
+    closeDialog() {
+      this.setEvent(null);
+    },
+  },
+};
+</script>
