7bb73e025ae84067f2d2ab03482d7c364bd3366d
nyohoo <nyohoo2013@gmail.com>
2022-06-19 01:29:55 +0900

    add: EditModeの判定をフォームコンポーネントに追加

--- frontend/src/components/EventFormDialog.vue
+++ frontend/src/components/EventFormDialog.vue
@@ -10,6 +10,9 @@
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
     </v-card-text>
+    <v-card-actions class="d-flex justify-end">
+      <v-btn @click="submit">保存</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -29,8 +32,9 @@ export default {
     ...mapGetters('events', ['event']),
   },
   methods: {
-    ...mapActions('events', ['setEvent']),
+    ...mapActions('events', ['setEvent', 'setEditMode']),
     closeDialog() {
+      this.setEditMode(false);
       this.setEvent(null);
     },
   },
