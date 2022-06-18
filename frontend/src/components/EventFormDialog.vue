4e9423af84c6bad5b0796eef88c82b04ec5648f6
nyohoo <nyohoo2013@gmail.com>
2022-06-19 01:32:00 +0900

    fix: 次のコミットのために調整

--- frontend/src/components/EventFormDialog.vue
+++ frontend/src/components/EventFormDialog.vue
@@ -10,9 +10,9 @@
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
     </v-card-text>
-    <v-card-actions class="d-flex justify-end">
+    <!-- <v-card-actions class="d-flex justify-end">
       <v-btn @click="submit">保存</v-btn>
-    </v-card-actions>
+    </v-card-actions> -->
   </v-card>
 </template>
 
