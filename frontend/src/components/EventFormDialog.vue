a76d960f44bb0fd20aee9909ab74c7bd0ae47ee2
nyohoo <nyohoo2013@gmail.com>
2022-06-19 03:46:50 +0900

    add: イベント作成フィームにdate-pickerを利用

--- frontend/src/components/EventFormDialog.vue
+++ frontend/src/components/EventFormDialog.vue
@@ -9,6 +9,15 @@
       <DialogSection icon="mdi-square" :color="event.color || 'blue'">
         <v-text-field v-model="name" label="タイトル"></v-text-field>
       </DialogSection>
+      <DialogSection icon="mdi-clock-outline">
+        <v-date-picker 
+          v-model="startDate"
+          no-title
+          locale="ja-ja"
+          :day-format="startDate => new Date(startDate).getDate()"
+        ></v-date-picker>
+        <p>{{ startDate }}</p>
+      </DialogSection>
     </v-card-text>
     <v-card-actions class="d-flex justify-end">
       <v-btn @click="submit">保存</v-btn>
@@ -27,6 +36,7 @@ export default {
   },
   data: () => ({
     name: '',
+    startDate: null,
   }),
   computed: {
     ...mapGetters('events', ['event']),
@@ -40,7 +50,7 @@ export default {
     submit() {
       const params = {
         name: this.name,
-        start: this.event.start,
+        start: this.startDate,
         end: this.event.end,
       };
       this.createEvent(params);
