0ac7c6e351979702629b77f076ef5422f78e80da
nyohoo <nyohoo2013@gmail.com>
2022-06-22 22:59:51 +0900

    add: 予定詳細ダイアログにカレンダーを表示する

--- frontend/src/components/events/EventDetailDialog.vue
+++ frontend/src/components/events/EventDetailDialog.vue
@@ -28,7 +28,9 @@
         {{ event.description || 'no description' }}
       </DialogSection>
     </v-card-text>
-
+    <v-card-text>
+      <DialogSection icon="mdi-calendar">{{ event.calendar.name }}</DialogSection>
+    </v-card-text>
   </v-card>
 </template>
 
