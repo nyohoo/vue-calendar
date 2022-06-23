eacdb587add3e1becbec9df10ef41d42dd8fbd62
nyohoo <nyohoo2013@gmail.com>
2022-06-23 10:57:37 +0900

    add:Vuexストアにカレンダーの作成アクションを追加

--- frontend/src/store/modules/calendars.js
+++ frontend/src/store/modules/calendars.js
@@ -5,21 +5,32 @@ const apiUrl = 'http://localhost:3000';
 
 const state = {
   calendars: [],
+  calendar: null,
 };
 
 const getters = {
   calendars: state => state.calendars.map(calendar => serializeCalendar(calendar)),
+  calendar: state => serializeCalendar(state.calendar),
 };
 
 const mutations = {
   setCalendars: (state, calendars) => (state.calendars = calendars),
+  appendCalendar: (state, calendar) => (state.calendars = [...state.calendars, calendar]),
+  setCalendar: (state, calendar) => (state.calendar = calendar),
 };
-
+  
 const actions = {
   async fetchCalendars({ commit }) {
     const response = await axios.get(`${apiUrl}/calendars`);
     console.log(response);
     commit('setCalendars', response.data);
+  },
+  async createCalendar({ commit }, calendar) {
+    const response = await axios.post(`${apiUrl}/calendars`, calendar);
+    commit('appendCalendar', response.data);
+  },
+  setCalendar({ commit }, calendar) {
+    commit('setCalendar', calendar);
   },
 };
 
